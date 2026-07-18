416e67766fd9518ddf92ef6470247c656d8816b6
Roger H <henkelroger@yahoo.com>
2026-07-18 09:29:32 -0700

    Added missing segments for my labs

---
+++ Module 03/Labs/JSFund.js
@@ -0,0 +1,38 @@
+"" + 1 + 0 //*10*//
+"" - 1 + 0 //*-1*//
+true + false //*1*//
+!true //*false*//
+6 / "3" //*2*//
+"2" * "3" //*6*//
+4 + 5 + "px" //*"9px"//
+"$" + 4 + 5 //*"$45"*//
+"4" - 2 //*2*//
+"4px" - 2 //*NaN*//
+" -9 " + 5 //*"-9 5"//
+" -9 " - 5 //*-14*//
+null + 1 //*1*//
+undefined + 1 //*NaN*//
+undefined == null //*true*//
+undefined === null //*false*//
+" \t \n" - 2 //*-2*//
+
+let three = "3"
+let four = "4"
+let thirty = "30"
+//what is the value of the following expressions?
+let addition = three + four
+let multiplication = three * four
+let division = three / four
+let subtraction = three - four
+let lessThan1 = three < four
+let lessThan2 = thirty < four
+
+
+if (0) console.log('#1 zero is true')
+if ("0") console.log('#2 zero is true')
+if (null) console.log('null is true')
+if (-1) console.log('negative is true')
+if (1) console.log('positive is true')
+
+
+    
